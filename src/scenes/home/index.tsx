31e295dfe5f0d51b0e38b629cd2e3e1cef65fb53
Mimisax <wiseonu@gmail.com>
2024-01-12 14:05:07 +0100

    second commit

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -11,7 +11,7 @@ type Props = {
 const Home = ({ setselectedPage }: Props) => {
     const isAboveMeduimScreens = useMediaQuery ("(min-width: 1060px)");
   return <section id = "Home" className="gap-16 bg-slate-100 py-10 md:h-full md:pb-0">
-    <motion.div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6" onViewportEnter={() => setselectedPage(SelectedPage.Home)}>
+    <motion.div className="md:flex mx-auto flex  w-5/6 items-center justify-center md:h-5/6" onViewportEnter={() => setselectedPage(SelectedPage.Home)}>
 
        <motion.div className="z-10 mt-32 md:basis-3/5" initial= "hidden" whileInView="visible" viewport={{once: true, amount: 0.5,}} transition={{duration: 1}} variants={{hidden:{opacity:0, x:-300}, visible:{opacity: 1, x:0}}}>
           <div className="md:-mt-20">
@@ -23,7 +23,7 @@ const Home = ({ setselectedPage }: Props) => {
                   Hello!! Welcome to my Photography World.</p>
        </motion.div>
        
-     <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
+     <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end max-h-96">
       <img src={Miracle} />
       
     </div>
